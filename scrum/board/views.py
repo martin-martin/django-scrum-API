21bdb52bd79475e25228c13ecff32089f77130f6
martin-martin <breuss.martin@gmail.com>
2018-09-27 23:03:32 +0200

    create serializers

--- scrum/board/views.py
+++ scrum/board/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import authentication, permissions, viewsets
+from .models import Sprint
+from .serializers import SprintSerializer
 
-# Create your views here.
+
+class DefaultsMixin(object):
+    """Default settings for view authentication, permissions, filtering
+    and pagination."""
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+
+# the ModelViewSet provides the necessary scaffolding for CRUD operations
+# with the HTTP verbs we got to know. There's no need to define it here
+# http://www.django-rest-framework.org/
+# Make sure to inherit from both classes!
+class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """API endpoint for listening and creating sprints."""
+
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
