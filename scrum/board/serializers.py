b56d9a4c20405a81d3fc0605c990e863adbfb8ff
martin-martin <breuss.martin@gmail.com>
2018-09-27 23:12:07 +0200

    add Task serializer

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Sprint
+from .models import Sprint, Task
 
 
 class SprintSerializer(serializers.ModelSerializer):
@@ -8,3 +8,18 @@ class SprintSerializer(serializers.ModelSerializer):
         model = Sprint
         # exposes all fields in the API
         fields = ('id', 'name', 'description', 'end', )
+
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    assigned = serializers.SlugRelatedField(
+        slug_field=User.USERNAME_FIELD, required=False)
+    status_display = serializers.SerializerMethodField('get_status_display')
+
+    class Meta:
+        model = Task
+        fields = ('id', 'name', 'description', 'status', 'order',
+                  'assigned', 'started', 'due', 'completed')
+
+    def get_status_display(self, obj):
+        return obj.get_status_display()
