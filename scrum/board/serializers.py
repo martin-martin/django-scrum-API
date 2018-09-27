9b10eded7f8b906d433b5091538c0a4e230a6d7b
martin-martin <breuss.martin@gmail.com>
2018-09-27 23:56:45 +0200

    create hypermedia links

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from .models import Sprint, Task
 
 
@@ -9,18 +10,39 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
 
     full_name = serializers.CharField(source='get_full_name', read_only=True)
+    links = serializers.SerializerMethodField('get_links')
 
     class Meta:
         model = User
         fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', )
 
+    def get_links(self, obj):
+        request = self.context['request']
+        username = obj.get_username()
+
+        return {
+            'self': reverse('user-detail',
+                            kwargs={User.USERNAME_FIELD: username},
+                            request=request),
+        }
+
 
 class SprintSerializer(serializers.ModelSerializer):
+
+    links = serializers.SerializerMethodField('get_links')
 
     class Meta:
         model = Sprint
         # exposes all fields in the API
         fields = ('id', 'name', 'description', 'end', )
+
+    def get_links(self, obj):
+        request = self.context['request']
+
+        return {
+            'self': reverse('sprint-detail',
+                            kwargs={'pk': obj.pk}, request=request),
+        }
 
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -28,6 +50,7 @@ class TaskSerializer(serializers.ModelSerializer):
     assigned = serializers.SlugRelatedField(
         slug_field=User.USERNAME_FIELD, required=False)
     status_display = serializers.SerializerMethodField('get_status_display')
+    links = serializers.SerializerMethodField('get_links')
 
     class Meta:
         model = Task
@@ -36,3 +59,21 @@ class TaskSerializer(serializers.ModelSerializer):
 
     def get_status_display(self, obj):
         return obj.get_status_display()
+
+    def get_links(self, obj):
+        request = self.context['request']
+        links = {
+            'self': reverse('task-detail',
+                            kwargs={'pk': obj.pk}, request=request),
+            'sprint': None,
+            'assigned': None,
+        }
+        if obj.sprint_id:
+            links['sprint'] = reverse('sprint-detail',
+                                      kwargs={'pk': obj.sprint_id},
+                                      request=request, )
+        if obj.assigned:
+            links['assigned'] = reverse('user-detail',
+                                        kwargs={User.USERNAME_FIELD: obj.assigned},
+                                        request=request, )
+        return links
