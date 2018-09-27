8a4fd6f3203f2a7b91d3a3e91e5548063bd9340a
martin-martin <breuss.martin@gmail.com>
2018-09-28 00:08:03 +0200

    fix: add fields, remove redundant function names

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -10,11 +10,11 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
 
     full_name = serializers.CharField(source='get_full_name', read_only=True)
-    links = serializers.SerializerMethodField('get_links')
+    links = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', )
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', 'links', )
 
     def get_links(self, obj):
         request = self.context['request']
@@ -29,12 +29,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 class SprintSerializer(serializers.ModelSerializer):
 
-    links = serializers.SerializerMethodField('get_links')
+    links = serializers.SerializerMethodField()
 
     class Meta:
         model = Sprint
         # exposes all fields in the API
-        fields = ('id', 'name', 'description', 'end', )
+        fields = ('id', 'name', 'description', 'end', 'links', )
 
     def get_links(self, obj):
         request = self.context['request']
@@ -51,13 +51,14 @@ class TaskSerializer(serializers.ModelSerializer):
     # https://stackoverflow.com/questions/27484344/assertion-error-at-django-rest-framework
     assigned = serializers.SlugRelatedField(
         slug_field=User.USERNAME_FIELD, required=False, read_only=True)
-    status_display = serializers.SerializerMethodField('get_status_display')
-    links = serializers.SerializerMethodField('get_links')
+    status_display = serializers.SerializerMethodField()
+    links = serializers.SerializerMethodField()
 
     class Meta:
         model = Task
         fields = ('id', 'name', 'description', 'status', 'order',
-                  'assigned', 'started', 'due', 'completed', )
+                  'assigned', 'started', 'due', 'completed', 'status_display',
+                  'links', )
 
     def get_status_display(self, obj):
         return obj.get_status_display()
