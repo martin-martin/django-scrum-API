c5cbd5e7e063509c61e38cb76213cc536c4d035e
martin-martin <breuss.martin@gmail.com>
2018-09-28 10:29:39 +0200

    fix: filter_name in django-filters 2.0

--- scrum/board/forms.py
+++ scrum/board/forms.py
@@ -1,5 +1,9 @@
 import django_filters
+from django.contrib.auth import get_user_model
 from .models import Task
+
+
+User = get_user_model()
 
 
 class NullFilter(django_filters.BooleanFilter):
@@ -7,14 +11,22 @@ class NullFilter(django_filters.BooleanFilter):
 
     def filter(self, qs, value):
         if value is not None:
-            return qs.filter(**{f'{self.name}__isnull': value})
+            # attention: see changes in comment below ('field_name')
+            return qs.filter(**{f'{self.field_name}__isnull': value})
         return qs
 
 
 class TaskFilter(django_filters.FilterSet):
 
-    backlog = NullFilter(name='sprint')
+    # django-filters 2.0 has changed 'name' to 'field_name'!
+    # https://django-filter.readthedocs.io/en/master/guide/migration.html#filter-name-renamed-to-filter-field-name-792
+    backlog = NullFilter(field_name='sprint')
 
     class Meta:
         model = Task
         fields = ('sprint', 'status', 'assigned', )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filters['assigned'].extra.update(
+            {'to_field_name': User.USERNAME_FIELD})
