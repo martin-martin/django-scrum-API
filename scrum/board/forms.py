7e67d65bfea89e0fe55039cacb135084f8d5eae3
martin-martin <breuss.martin@gmail.com>
2018-09-28 10:02:53 +0200

    allow filtering for backlog tasks

--- scrum/board/forms.py
+++ scrum/board/forms.py
@@ -2,6 +2,15 @@ import django_filters
 from .models import Task
 
 
+class NullFilter(django_filters.BooleanFilter):
+    """Filter on a field set as null or not."""
+
+    def filter(self, qs, value):
+        if value is not None:
+            return qs.filter(**{f'{self.name}__isnull': value})
+        return qs
+
+
 class TaskFilter(django_filters.FitlerSet):
 
     class Meta:
